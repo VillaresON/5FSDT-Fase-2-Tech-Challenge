5c8061229c79747e1c76841a57828496d7d8702e
Jonathas Villares <113390493+VillaresON@users.noreply.github.com>
2025-07-21 22:32:18 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,8 @@ const post = require('../routes/postRouter.js');
 const autor = require('../routes/autorRouter.js');
 
 module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.redirect('http://localhost:3000/api-docs/');
+    });
     app.use(express.json(), post, autor);
 }
